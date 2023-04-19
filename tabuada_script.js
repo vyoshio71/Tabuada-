067d9ec14cc8b3e3bb8938d94fa16a06532bc969
Victor Yoshio Tanaka <116774749+vyoshio71@users.noreply.github.com>
2023-04-19 17:20:49 -0300

    Add files via upload

---
+++ tabuada_script.js
@@ -0,0 +1,19 @@
+function Tabuada() {
+    var num = window.document.getElementById("num");
+    var end = document.getElementById("end");
+    if (num.value.length == 0 || end.value.length == 0) {
+      window.alert("[ERRO] Confira os dados!");
+    } else {
+          var n = Number(num.value);
+          var e = Number(end.value);
+          var x = 0;
+          var r = "";
+          var res = window.document.getElementById("resposta");
+          res.innerHTML = `A tabuada do ${n} é: <br>`;
+          do {
+          r = n * x;
+          res.innerHTML += `${x} x ${n} = ${r} <br>`;
+          x++;
+          } while (x <= e);
+      }
+  }
